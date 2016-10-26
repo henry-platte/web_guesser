dec0b69f0d37a2762caddcfd6a85432261c35fd1
Jacob Ritari <jritari@gm.slc.edu>
2016-10-26 17:15:08 -0400

    It finally f$&##% works!

--- web_guesser.rb
+++ web_guesser.rb
@@ -2,5 +2,12 @@ require 'sinatra'
 require 'sinatra/reloader'
 get '/' do
   number = rand(100)
-  erb :index, :locals => {:number => number}
+  guess = params['guess'].to_i
+  if guess > number
+    message = "Too high!"
+  elsif number > guess
+    message = "Too low!"
+  else message = "You're right!"
+end
+  erb :index, :locals => {:number => number, :message => message}
 end
